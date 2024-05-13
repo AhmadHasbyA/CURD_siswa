a177d6bd5ecd7472822837e90d2fa54c6408e85f
ahmad hasby assidiqi <echo.ahmadhasyassidiqi@gmail.com>
2024-05-13 21:14:23 +0700

    update

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -5,6 +5,8 @@ function Home() {
   return (
     <>
       <div className=" bg-black text-white w-full h-screen gap-5 flex justify-center items-center">
+        <div>SEkolah</div>
+
         <Link to="/daftar">
           <button className="bg-[red] flex justify-center items-center rounded-md w-[200px] h-[50px]">Daftar Siswa</button>
         </Link>
